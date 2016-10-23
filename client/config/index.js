ff2fd7f75406dc4444956e1f188b08ef11279e3c
daihere <daihere1993@gmail.com>
2016-10-23 22:31:22 +0800

    修改client -> server 的配置

--- client/config/index.js
+++ client/config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    productionSourceMap: true,
+    assetsPublicPath: './',
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
@@ -18,15 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
     proxyTable: {},
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    assetsPublicPath: '/'
   }
 }
--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,3 +1,14 @@
-module.exports = {
-  NODE_ENV: '"production"'
+var fs = require('fs')
+var merge = require('webpack-merge')
+config = {
+  NODE_ENV: '"production"',
+  //填写生产环境时,该博客的访问地址
+  index: '"http://localhost"',
+  api: '"http://localhost:3000/api/"'
 }
+if (process.env.NODE_ENV === 'production'){
+  if (fs.existsSync(__dirname + '/private.js')){
+    config = merge(config, require('./private.js'))
+  }
+}
+module.exports = config
