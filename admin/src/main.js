677b650442b34b7a6d149b506478fd55c1b15ec5
daihere <daihere1993@gmail.com>
2016-11-14 00:37:49 +0800

    [代码优化]使用了自己在npm上的package:vue-alert-osd,删除了Alert组件

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,10 +1,11 @@
 import './stylus/index.styl'
 import Vue from 'vue'
-import Alert from './js/Alert'
+import Alert from 'vue-alert-osd'
 import adminComponent from './components/Admin.vue'
 import { router } from './router.js'
 import { date, md2text } from './filters'
 require('font-awesome/css/font-awesome.min.css')
+require('vue-alert-osd/lib/vue-alert.css')
 
 Alert.set('duration', 3000)
 window.alert = Alert
