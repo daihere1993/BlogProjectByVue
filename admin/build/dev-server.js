797bd42be83cd2737620eae4537f3ffa80860005
daihere <daihere1993@gmail.com>
2016-12-15 22:16:15 +0800

    [缺陷修改]解决静态资源获取不到的缺陷

--- admin/build/dev-server.js
+++ admin/build/dev-server.js
@@ -54,8 +54,7 @@ app.use(hotMiddleware)
 
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
-
+app.use(staticPath, express.static('static'))
 module.exports = app.listen(port, function (err) {
   if (err) {
     console.log(err)
