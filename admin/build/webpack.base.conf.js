366ed5176817dc0eb58c38262b54950843ad1bf6
daihere <daihere1993@gmail.com>
2017-04-24 11:25:30 +0800

    更新vue-alert-osd的版本

--- admin/build/webpack.base.conf.js
+++ admin/build/webpack.base.conf.js
@@ -2,7 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
-console.log(config.build.assetsPublicPath)
+
 module.exports = {
   entry: {
     app: './admin/src/main.js'
