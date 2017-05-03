5c240820085c72d07c85a2979a975c527cbbfaa1
daihere <daihere1993@gmail.com>
2017-05-03 10:22:19 +0800

    [缺陷修改]修改dateUtil.format

--- admin/src/lib/dateUtil.js
+++ admin/src/lib/dateUtil.js
@@ -1,44 +1,18 @@
 var dateUtil = {
-  format: function(date, format) {
-    if (!date)
-      return "";
-
-    if (format === "pithy")
-    {
-      var now= new Date();
-      var short = $filter("date")(date,"shortDate");
-      var shortA = $filter("date")(now,"shortDate");
-      var minus = now.getTime() - date.getTime();
-      var residue = Math.floor(minus/86400000);
-      var Y=$filter("date")(now,"yyyy");
-      var y=$filter("date")(date,"yyyy");
-      var M=$filter("date")(date,"MM");
-      var D=$filter("date")(date,"dd");
-      var H=$filter("date")(date,"HH");
-      var m=$filter("date")(date,"mm");
-      if (short == shortA){
-        return "今天 "+H+":"+m;
-      }
-      else if(residue == 0){
-        return "昨天 "+H+":"+m;
-      }
-      else if(0<residue && residue<7){
-        return residue+"天前";
-      }
-      else if(Y == y){
-        return  M+"月"+D+"日";
-      }else{
-        return y+"年"+M+"月"+D+"日";
-      }
-    }
-
-    if (format === "date")
-      format = "yyyy/MM/dd";
-    else if (format === "time")
-      format = "HH:mm:ss";
-    else if (format === "dateTime" || !format)
-      format = "yyyy-MM-dd HH:mm:ss";
-    return _dateFormat(date, format);
+  format: function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
   }
 }
 
