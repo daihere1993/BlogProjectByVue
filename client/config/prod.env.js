90e15a800ca1a79c8be8302fae2f4ffaa8c093c8
daihere <daihere1993@gmail.com>
2017-05-04 11:49:47 +0800

    [代码优化]

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -4,8 +4,7 @@ config = {
   NODE_ENV: '"production"',
   //填写生产环境时,该博客的访问地址
   index: '"http://localhost"',
-  api: '"https://justforlife.cn/api/"',
-  duoshuoShortName: '"comment"'
+  api: '"https://justforlife.cn/api/"'
 }
 if (process.env.NODE_ENV === 'production'){
   if (fs.existsSync(__dirname + '/private.js')){
