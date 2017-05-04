37c62c322f2e7159ba0b75d6a72bba256e5e4f57
daihere <daihere1993@gmail.com>
2017-05-04 15:35:51 +0800

    [样式修改]修改导航栏样式

--- client/src/components/common/HeaderNav.vue
+++ client/src/components/common/HeaderNav.vue
@@ -1,28 +1,36 @@
 <template>
-  <header class="top-nav-container">
-    <nav class="top-nav">
-      <a href="javascript:;" class="vertical-center nav-logo">
-        <span class="brand vertical-center-content">自制与光阴</span>
-      </a>
-      <ul class="nav-link-container">
-        <li class="nav-link-item">
-          <router-link class="nav-link" to="/posts">文章</router-link>
-        </li>
-        <li class="nav-link-item">
-          <router-link class="nav-link" to="/tags">标签</router-link>
-        </li>
-        <li class="nav-link-item">
-          <router-link class="nav-link" to="/me">关于我</router-link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <div class="header">
+    <header class="top-nav-container">
+      <nav class="top-nav">
+        <a href="javascript:;" class="vertical-center nav-logo">
+          <span class="brand vertical-center-content">自制与光阴</span>
+        </a>
+        <ul class="nav-link-container">
+          <li class="nav-link-item">
+            <router-link class="nav-link" to="/posts">文章</router-link>
+          </li>
+          <li class="nav-link-item">
+            <router-link class="nav-link" to="/tags">标签</router-link>
+          </li>
+          <li class="nav-link-item">
+            <router-link class="nav-link" to="/me">关于我</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="after-header-box">
+      <div class="after-header-box-gap triangle-up"></div>
+    </div>
+  </div>
+    
 </template>
 <style lang="stylus">
   @import "../../stylus/_settings.styl"
   $header-height = 40px
+  $after-header-box-height = 14px;
   .top-nav-container
     box-shadow 0 0 4px rgba(0,0,0,0.25)
+    background $dark
   .top-nav
     max-width  850px
     margin 0 auto
@@ -36,8 +44,8 @@
     width 40px
     height 40px
   .brand
-    font-size 1.5em
-    color $dark
+    font-size 2.5em
+    color $snow
     font-family $logo-font
     font-weight 500
   /**
@@ -57,13 +65,25 @@
     display inline-block
     margin 0 .6em
   .nav-link
+    font-size: 15px;
     height $header-height
     line-height $header-height
     text-decoration none
-    color $medium
+    color $snow
     padding-bottom 3px
     &:hover, &.active
         border-bottom 3px solid $green
+  .after-header-box
+    height $after-header-box-height
+    background $green
+  .triangle-up
+    width: 0
+    height 0
+    border-left 7px solid transparent
+    border-right 7px solid transparent
+    border-bottom $after-header-box-height solid $snow
+  .after-header-box-gap
+    margin 0 auto;
 </style>
 <script>
   export default{
