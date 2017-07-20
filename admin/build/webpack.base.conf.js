2e7113a4817739d68e2390f756731689fa47e1ae
daihere <daihere1993@gmail.com>
2017-07-20 09:38:52 +0800

    [缺陷修改]webpack.base.conf.js中simpleMDE引用路径错误

--- admin/build/webpack.base.conf.js
+++ admin/build/webpack.base.conf.js
@@ -17,9 +17,9 @@ module.exports = {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
       'components': path.resolve(__dirname, '../src/components'),
-      'SimpleMDE': 'simplemde/dist/simplemde.min.js',
+      'SimpleMDE': 'simplemde-extend/dist/simplemde.min.js',
       'github-markdown-css': 'github-markdown-css/github-markdown.css',
-      'simpleMDEStyle': 'simplemde/dist/simplemde.min.css'
+      'simpleMDEStyle': 'simplemde-extend/dist/simplemde.min.css'
     }
   },
   externals: {
